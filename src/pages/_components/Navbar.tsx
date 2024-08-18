1b709fe02cf923158830053997a22c7f1531fecf
cunev <cunevnew@gmail.com>
2024-08-18 13:57:08 -0700

    feat: search

--- src/pages/_components/Navbar.tsx
+++ src/pages/_components/Navbar.tsx
@@ -1,10 +1,46 @@
+import { Button } from "@/shadcn/ui/button";
+import {
+  Command,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/shadcn/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/shadcn/ui/popover";
+import { CommandLoading } from "cmdk";
+import { Search, User } from "lucide-react";
+import { useEffect, useState } from "react";
 import { BiLogIn, BiLogOut } from "react-icons/bi";
 import { Link, useNavigate } from "react-router-dom";
+import { searchUsers } from "rhythia-api";
+import { useDebounce } from "use-debounce";
 import { supabase, useProfile } from "../../supabase";
-
 export function Navbar() {
   const { user } = useProfile();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState("");
   const navigate = useNavigate();
+  const [users, setUsers] = useState<Awaited<ReturnType<typeof searchUsers>>>(
+    {}
+  );
+  const [debounced] = useDebounce(value, 500);
+  useEffect(() => {
+    async function search() {
+      if (debounced == "") {
+        setUsers({});
+        setLoading(false);
+
+        return;
+      }
+      setUsers(await searchUsers({ text: debounced }));
+      setLoading(false);
+    }
+    search();
+  }, [debounced]);
+
+  useEffect(() => {
+    if (value.length) setLoading(true);
+  }, [value]);
   return (
     <div
       className="w-full h-[60px] bg-repeat bg-center  bg-[length:72px_100px] shadow-md flex justify-center items-center hue-rotate-[45deg]"
@@ -31,10 +67,59 @@ export function Navbar() {
 
         <div className="flex gap-10 text-sm text-white font-medium justify-center items-center h-full ml-auto">
           <div className="flex gap-2 h-full">
-            <input
-              className="border-[1px] px-4 my-2 rounded-md text-xs border-[#6220EC] flex items-center gap-3 bg-transparent outline-none"
-              placeholder="Search players"
-            ></input>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="border-[1px] px-4 my-2 rounded-md text-xs border-[#6220EC] flex items-center gap-3 bg-transparent outline-none h-8 mr-2 hover:bg-transparent w-[200px]"
+                >
+                  Search users
+                  <Search className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command className="bg-transparent ">
+                  <CommandInput
+                    value={value}
+                    onInput={async (event) => {
+                      setValue((event.target as HTMLInputElement).value);
+                    }}
+                    placeholder="Type a username..."
+                  />
+                  <CommandList>
+                    {loading ? (
+                      <CommandLoading className="h-10 flex items-center px-4">
+                        Loading..
+                      </CommandLoading>
+                    ) : (
+                      users.results &&
+                      users.results.map((user) => (
+                        <CommandItem
+                          className="hover:bg-transparent"
+                          key={`word-${user.username}`}
+                          value={user.username || ""}
+                        >
+                          <Button
+                            className="bg-transparent text-white hover:bg-transparent w-full h-6 flex justify-start"
+                            onClick={() => {
+                              navigate(`/player/${user.id}`);
+                              setOpen(false);
+                              setValue("");
+                            }}
+                          >
+                            <User className="mr-2 h-4 w-4" />
+                            <span>{user.username}</span>
+                          </Button>
+                        </CommandItem>
+                      ))
+                    )}
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+
             <img src={"/search.png"} alt="" width={44} height={44} />
           </div>
 
