417734a7cce7ab3cc4cab191789cd783099b0565
cunev <cunevnew@gmail.com>
2024-10-11 10:37:06 -0700

    ch: remove page numbers, pagination todo/

--- src/pages/leaderboards/_components/pagiantions.tsx
+++ src/pages/leaderboards/_components/pagiantions.tsx
@@ -25,25 +25,6 @@ export default function Pagination({
         <PaginationItem>
           <PaginationPrevious href={`?page=${Math.max(currentPage - 1, 1)}`} />
         </PaginationItem>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <PaginationItem key={index}>
-            <PaginationLink href={`?page=${index + 1}`}>
-              {index + 1}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
-        {totalPages > 5 && (
-          <>
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href={`?page=${totalPages}`}>
-                {totalPages}
-              </PaginationLink>
-            </PaginationItem>
-          </>
-        )}
         <PaginationItem>
           <PaginationNext
             href={`?page=${Math.min(currentPage + 1, totalPages)}`}
