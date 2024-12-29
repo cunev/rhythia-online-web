fe21bf1f444aab02720c335a4f345f312f85a08a
cunev <cunevnew@gmail.com>
2024-12-29 17:20:49 +0200

    feat: clan edits

--- src/pages/clans/[id]/index.tsx
+++ src/pages/clans/[id]/index.tsx
@@ -9,14 +9,31 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/shadcn/ui/alert-dialog";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/shadcn/ui/card";
-import { getJwt } from "@/supabase";
+import { Card, CardHeader } from "@/shadcn/ui/card";
+import { getJwt, useProfile } from "@/supabase";
 import { LoaderData } from "@/types";
 import { useState } from "react";
 import Markdown from "react-markdown";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import remarkGfm from "remark-gfm";
-import { getClan } from "rhythia-api";
+import { editClan, getAvatarUploadUrl, getClan } from "rhythia-api";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shadcn/ui/form";
+import { toast } from "@/shadcn/ui/use-toast";
+import { Input } from "@/shadcn/ui/input";
+import { Button } from "@/shadcn/ui/button";
+import { Textarea } from "@/shadcn/ui/textarea";
+import { Label } from "@/shadcn/ui/label";
+import { getIntrinsicToken } from "@/pages/_components/IntrinsicGen";
 
 export const Loader = async ({ params }: any) => {
   const jwt = await getJwt();
@@ -33,10 +50,21 @@ const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 export default function SpecificClan() {
   const loaderData = useLoaderData() as LoaderData<typeof Loader>;
   const [isOpen, setIsOpen] = useState(false);
+  const [isEditOpen, setEditOpen] = useState(false);
   const [pendingUrl, setPendingUrl] = useState("");
-
+  const profile = useProfile();
   const clan = loaderData.getClan;
-  console.log(loaderData);
+  const [avatar, setAvatar] = useState(clan.avatar_url);
+  const navigate = useNavigate();
+  const form = useForm({
+    defaultValues: {
+      clanName: clan.name,
+      clanAvatar: clan.avatar_url,
+      clanAcronym: clan.acronym,
+      clanDescription: clan.description,
+    },
+  });
+
   return (
     <>
       <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
@@ -63,6 +91,177 @@ export default function SpecificClan() {
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
+
+      <AlertDialog open={isEditOpen} onOpenChange={setEditOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Edit Clan</AlertDialogTitle>
+            <AlertDialogDescription>
+              Any modifications such as names, acronyms can be taken by other
+              clans after change.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(async (form) => {
+                const res = await editClan({
+                  session: await getJwt(),
+                  acronym: form.clanAcronym,
+                  name: form.clanName,
+                  avatar_url: form.clanAvatar,
+                  description: form.clanDescription || "",
+                  id: clan.id,
+                });
+
+                if (res.error) {
+                  toast({
+                    title: "Oops",
+                    description: res.error,
+                    variant: "destructive",
+                  });
+                  return;
+                }
+                setEditOpen(false);
+                location.reload();
+              })}
+              className="space-y-2"
+            >
+              <div className="w-full justify-center flex">
+                <img
+                  src={avatar || ""}
+                  className="rounded-full border-8 h-20 w-20"
+                  onError={(event) => {
+                    (
+                      event.target as HTMLImageElement
+                    ).src = `https://a.ppy.sh/u/1`;
+                  }}
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="name">Avatar</Label>
+                <Input
+                  id="name"
+                  type="file"
+                  accept=".png, .jpg, .jpeg, .webp"
+                  className="col-span-5 file:text-white text-transparent"
+                  onChange={async (e) => {
+                    const file = e.target.files?.[0];
+                    if (file) {
+                      const reader = new FileReader();
+                      reader.onload = async (e) => {
+                        const intrinsicToken = await getIntrinsicToken();
+                        const res = await getAvatarUploadUrl({
+                          session: await getJwt(),
+                          contentLength: (e.target?.result as ArrayBuffer)
+                            .byteLength,
+                          contentType: "image/jpeg",
+                          intrinsicToken,
+                        });
+
+                        await fetch(res.url!, {
+                          method: "PUT",
+                          body: e.target?.result as ArrayBuffer,
+
+                          headers: {
+                            "Content-Type": "image/jpeg",
+                          },
+                        });
+
+                        form.setValue(
+                          "clanAvatar",
+                          `https://static.rhythia.com/${res.objectKey}`
+                        );
+                        setAvatar(
+                          `https://static.rhythia.com/${res.objectKey}`
+                        );
+                      };
+                      reader.readAsArrayBuffer(file);
+                    }
+                  }}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="clanName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Full Clan Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="My Amazing Clan" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This name will be displayed only in the clan page
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="clanAcronym"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Clan Acronym</FormLabel>
+                    <FormControl>
+                      <Input placeholder="MAC" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This acronym will be displayed along you player name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="clanDescription"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="MAC" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      The description of your clan, you can use Markdown.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex gap-2 w-full justify-end">
+                <Button type="submit">Edit Clan</Button>
+                <Button
+                  type="reset"
+                  variant={"outline"}
+                  onClick={() => {
+                    form.reset();
+                    setEditOpen(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </AlertDialogContent>
+      </AlertDialog>
+      <div className="flex gap-2 justify-end mb-4">
+        <Button
+          disabled={
+            !profile.userProfile || profile.userProfile.id !== clan.owner
+          }
+          onClick={() => {
+            setEditOpen(true);
+          }}
+        >
+          Edit clan
+        </Button>
+      </div>
+
       <div className="grid grid-cols-3 gap-4 items-start">
         <div className="flex flex-col gap-4">
           <Card className="bg-neutral-900 shadow-md rounded-sm text-sm border-[1px] border-neutral-800">
@@ -96,7 +295,10 @@ export default function SpecificClan() {
 
           {clan.users.map((user) => (
             <div
-              className="flex flex-col items-end justify-between w-full bg-neutral-900 shadow-md drop-shadow-md rounded-sm p-2 text-sm border-[1px] border-neutral-800 gap-2 relative overflow-hidden bg-cover"
+              onClick={() => {
+                navigate("/player/" + user.id);
+              }}
+              className="cursor-pointer flex flex-col items-end justify-between w-full bg-neutral-900 shadow-md drop-shadow-md rounded-sm p-2 text-sm border-[1px] border-neutral-800 gap-2 relative overflow-hidden bg-cover"
               style={{ backgroundImage: `url(${user.profile_image})` }}
             >
               <div className="bg-gradient-to-b from-transparent  to-neutral-900 absolute top-0 left-0 w-full h-16 via-[#0a0a0a9f] -z-20"></div>
@@ -131,14 +333,9 @@ export default function SpecificClan() {
                   </div>
                 </div>
 
-                <div className="flex-col justify-end min-w-12 mr-1">
-                  <div className="z-40 text-white text-xl font-extrabold  text-right">
-                    {user.skill_points}{" "}
-                    <span className="text-sm font-medium">RP</span>
-                  </div>
-                  <div className="text-sm h-6 p-0 -mt-1 text-right">
-                    Place #{user.position}
-                  </div>
+                <div className="z-40 text-white text-xl font-extrabold  text-right">
+                  {user.skill_points}{" "}
+                  <span className="text-sm font-medium">RP</span>
                 </div>
               </div>
 
@@ -170,7 +367,7 @@ export default function SpecificClan() {
             <hr className="my-4" />
             <Markdown
               className={
-                "prose prose-sm dark:prose-invert prose-neutral dark max-h-96 overflow-y-scroll min-w-full prose-h1:mb-0 prose-h2:my-0 prose-h3:my-0 prose-h4:my-0 prose-li:my-0 prose-ol:m-0 prose-ul:m-0 overflow-hidden relative"
+                "prose prose-sm dark:prose-invert prose-neutral dark max-h-[1000px] overflow-y-scroll min-w-full prose-h1:mb-0 prose-h2:my-0 prose-h3:my-0 prose-h4:my-0 prose-li:my-0 prose-ol:m-0 prose-ul:m-0 overflow-hidden relative"
               }
               components={{
                 a: ({ node, ...props }) => (
