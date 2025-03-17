6832099f5ea0c041d76eb36742d405e41f6f76f4
cunev <cunevnew@gmail.com>
2025-03-17 10:03:51 +0200

    feat: set environment to production in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { Routes } from "@generouted/react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { setEnvironment } from "rhythia-api/handleApi";
-// setEnvironment("production");
+setEnvironment("production");
 createRoot(document.getElementById("root")!).render(<Routes />);
